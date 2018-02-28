76c14d59da9f7ed629c13a96b43e0040fd2fd4d7
ryan-parag <parag.ryan@gmail.com>
2018-02-28 11:45:28 -0500

    add architecture

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+(function () {
+	'use strict';
+}());
+
+var gulp = require('gulp'),
+	prefix = require('gulp-autoprefixer'),
+	sass = require('gulp-sass'),
+	minifyCSS = require('gulp-minify-css'),
+	sourcemaps  = require('gulp-sourcemaps');
+
+var paths = {
+  dist: './dist/',
+  sass: './src/scss/',
+  css: './dist/css/',
+	};
+
+/* SCSS
+==============================================*/
+gulp.task('sass', function () {
+	return gulp.src(paths.sass + 'styles.scss')
+		.pipe(sass())
+		.pipe(sourcemaps.init())
+		.pipe(minifyCSS())
+		.pipe(prefix())
+		.pipe(gulp.dest(paths.css));
+});
+
+/* Build
+==============================================*/
+gulp.task('build', ['sass']);
+
+/* Compile
+==============================================*/
+gulp.task('default', ['build']);
