316130f0aa84edb3565ff8f869b5284a2f08f2b7
ryan-parag <parag.ryan@gmail.com>
2018-03-13 16:32:50 -0400

    added demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-(function () {
-	'use strict';
-}());
+'use strict';
 
 var gulp = require('gulp'),
 	prefix = require('gulp-autoprefixer'),
 	sass = require('gulp-sass'),
 	minifyCSS = require('gulp-minify-css'),
 	sourcemaps  = require('gulp-sourcemaps'),
-	rename  = require('gulp-rename');
+	rename  = require('gulp-rename'),
+ pug = require('gulp-pug');
+
 
 var paths = {
   dist: './dist/',
   sass: './src/scss/',
   css: './dist/css/',
+		pug: './src/views/*.pug'
 	};
 
 /* SCSS
@@ -24,14 +25,37 @@ gulp.task('sass', function () {
 		.pipe(minifyCSS())
 		.pipe(prefix())
 		.pipe(rename({
-			prefix: 'demo-'
+			prefix: 'lido-'
 		}))
 		.pipe(gulp.dest(paths.css));
 });
 
+
+/* DEMO SCSS
+==============================================*/
+gulp.task('demo', function () {
+	return gulp.src(paths.sass + 'demo.scss')
+		.pipe(sass())
+		.pipe(sourcemaps.init())
+		.pipe(minifyCSS())
+		.pipe(prefix())
+		.pipe(rename({
+			prefix: 'styles-'
+		}))
+		.pipe(gulp.dest(paths.css));
+});
+
+/* PUG
+==============================================*/
+gulp.task('pug', function () {
+  return gulp.src(paths.pug)
+  .pipe(pug({}))
+.pipe(gulp.dest(paths.dist));
+});
+
 /* Build
 ==============================================*/
-gulp.task('build', ['sass']);
+gulp.task('build', ['sass', 'pug', 'demo']);
 
 /* Compile
 ==============================================*/
